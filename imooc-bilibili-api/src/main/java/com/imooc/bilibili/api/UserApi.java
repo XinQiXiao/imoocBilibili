f410afd8fd4095bee82e06a7259a3b7a28921dd0
qixin <379281867@qq.com>
2025-07-09 21:14:59 +0800

    3-10

--- imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/UserApi.java
+++ imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/UserApi.java
@@ -1,10 +1,13 @@
 package com.imooc.bilibili.api;
 
 import com.imooc.bilibili.domain.JsonResponse;
+import com.imooc.bilibili.domain.User;
 import com.imooc.bilibili.service.UserService;
 import com.imooc.bilibili.service.util.RSAUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +20,17 @@ public class UserApi {
     public JsonResponse<String> getRsaPulicKey(){
         String pk = RSAUtil.getPublicKeyStr();
         return new JsonResponse<>(pk);
+    }
+
+    @PostMapping("users")
+    public JsonResponse<String> addUser(@RequestBody User user){
+        userService.addUser(user);
+        return JsonResponse.success();
+    }
+
+    @PostMapping("/user-tokens")
+    public JsonResponse<String> login(@RequestBody User user){
+        String token = userService.login(user);
+        return new JsonResponse<>(token);
     }
 }
---
+++ imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/util/TokenUtil.java
@@ -0,0 +1,9 @@
+package com.imooc.bilibili.service.util;
+
+public class TokenUtil {
+
+    public String generateToken(Long userId){
+        String token = "123";
+        return token;
+    }
+}
