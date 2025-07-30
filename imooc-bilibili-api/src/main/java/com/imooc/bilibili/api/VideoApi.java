a02fd3f47365995c312b940beaee2d8d37f60315
qixin <379281867@qq.com>
2025-07-30 23:23:08 +0800

    4-17

--- imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/VideoApi.java
+++ imooc-bilibili-api/src/main/java/com/imooc/bilibili/api/VideoApi.java
@@ -8,6 +8,10 @@ import com.imooc.bilibili.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
 @RestController
 public class VideoApi {
 
@@ -36,6 +40,49 @@ public class VideoApi {
                                                           @RequestParam Integer no,
                                                           String area){
         PageResult<Video> result = videoService.pageListVideos(size, no ,area);
+        return new JsonResponse<>(result);
+    }
+
+    /**
+     * 视频在线播放
+     */
+    @GetMapping("/video-slices")
+    public void viewVideoOnlineBySlices(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        String url) {
+        videoService.viewVideoOnlineBySlices(request, response, url);
+    }
+
+    /**
+     * 点赞视频
+     */
+    @PostMapping("/video-likes")
+    public JsonResponse<String> addVideoLike(@RequestParam Long videoId){
+        Long userId = userSupport.getCurrentUserId();
+        videoService.addVideoLike(videoId, userId);
+        return JsonResponse.success();
+    }
+
+    /**
+     * 取消点赞视频
+     */
+    @DeleteMapping("/video-likes")
+    public JsonResponse<String> deleteVideoLike(@RequestParam Long videoId){
+        Long userId = userSupport.getCurrentUserId();
+        videoService.deleteVideoLike(videoId, userId);
+        return JsonResponse.success();
+    }
+
+    /**
+     * 查询视频点赞数量
+     */
+    @GetMapping("/video-likes")
+    public JsonResponse<Map<String, Object>> getVideoLikes(@RequestParam Long videoId){
+        Long userId = null;
+        try{
+            userId = userSupport.getCurrentUserId();
+        }catch (Exception ignored){}
+        Map<String, Object> result = videoService.getVideoLikes(videoId, userId);
         return new JsonResponse<>(result);
     }
 }
--- imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/VideoService.java
+++ imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/VideoService.java
@@ -2,17 +2,17 @@ package com.imooc.bilibili.service;
 
 import com.alibaba.fastjson.JSONObject;
 import com.imooc.bilibili.dao.VideoDao;
-import com.imooc.bilibili.domain.PageResult;
-import com.imooc.bilibili.domain.UserMoment;
-import com.imooc.bilibili.domain.Video;
-import com.imooc.bilibili.domain.VideoTag;
+import com.imooc.bilibili.domain.*;
 import com.imooc.bilibili.domain.constant.UserMomentsConstant;
 import com.imooc.bilibili.domain.exception.ConditionException;
+import com.imooc.bilibili.service.util.FastDFSUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -21,6 +21,9 @@ public class VideoService {
 
     @Autowired
     private VideoDao videoDao;
+
+    @Autowired
+    private FastDFSUtil fastDFSUtil;
 
     @Value("${fdfs.http.storage-addr}")
     private String fastdfsUrl;
@@ -62,5 +65,41 @@ public class VideoService {
                     .collect(Collectors.toSet());
         }
         return videoList;
+    }
+
+    public void viewVideoOnlineBySlices(HttpServletRequest request, HttpServletResponse response, String url) {
+        try{
+            fastDFSUtil.viewVideoOnlineBySlices(request, response, url);
+        }catch (Exception ignored){}
+    }
+
+    public void addVideoLike(Long videoId, Long userId) {
+        Video video = videoDao.getVideoById(videoId);
+        if(video == null){
+            throw new ConditionException("非法视频！");
+        }
+        VideoLike videoLike = videoDao.getVideoLikeByVideoIdAndUserId(videoId, userId);
+        if(videoLike != null){
+            throw new ConditionException("已经赞过！");
+        }
+        videoLike = new VideoLike();
+        videoLike.setVideoId(videoId);
+        videoLike.setUserId(userId);
+        videoLike.setCreateTime(new Date());
+        videoDao.addVideoLike(videoLike);
+    }
+
+    public void deleteVideoLike(Long videoId, Long userId) {
+        videoDao.deleteVideoLike(videoId, userId);
+    }
+
+    public Map<String, Object> getVideoLikes(Long videoId, Long userId) {
+        Long count = videoDao.getVideoLikes(videoId);
+        VideoLike videoLike = videoDao.getVideoLikeByVideoIdAndUserId(videoId, userId);
+        boolean like = videoLike != null;
+        Map<String, Object> result = new HashMap<>();
+        result.put("count", count);
+        result.put("like", like);
+        return result;
     }
 }
