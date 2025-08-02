078cb19b8165dafbb993fe8895673f815a106bd0
qixin <379281867@qq.com>
2025-08-02 19:30:34 +0800

    4-24

---
+++ imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/websocket/WebSocketService.java
@@ -0,0 +1,79 @@
+package com.imooc.bilibili.service.websocket;
+
+import com.imooc.bilibili.service.util.TokenUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+@ServerEndpoint("/imserver")
+public class WebSocketService {
+
+    private final Logger logger =  LoggerFactory.getLogger(this.getClass());
+
+    private static final AtomicInteger ONLINE_COUNT = new AtomicInteger(0);
+
+    public static final ConcurrentHashMap<String, WebSocketService> WEBSOCKET_MAP = new ConcurrentHashMap<>();
+
+    private Session session;
+
+    private String sessionId;
+
+    private Long userId;
+
+    private static ApplicationContext APPLICATION_CONTEXT;
+
+    public static void setApplicationContext(ApplicationContext applicationContext){
+        WebSocketService.APPLICATION_CONTEXT = applicationContext;
+    }
+
+    @OnOpen
+    public void openConnection(Session session, @PathParam("token") String token){
+        try{
+            this.userId = TokenUtil.verifyToken(token);
+        }catch (Exception ignored){}
+        this.sessionId = session.getId();
+        this.session = session;
+        if(WEBSOCKET_MAP.containsKey(sessionId)){
+            WEBSOCKET_MAP.remove(sessionId);
+            WEBSOCKET_MAP.put(sessionId, this);
+        }else{
+            WEBSOCKET_MAP.put(sessionId, this);
+            ONLINE_COUNT.getAndIncrement();
+        }
+        logger.info("用户连接成功：" + sessionId + "，当前在线人数为：" + ONLINE_COUNT.get());
+        try{
+            this.sendMessage("0");
+        }catch (Exception e){
+            logger.error("连接异常");
+        }
+    }
+
+    @OnClose
+    public void closeConnection(){
+        if(WEBSOCKET_MAP.containsKey(sessionId)){
+            WEBSOCKET_MAP.remove(sessionId);
+            ONLINE_COUNT.getAndDecrement();
+        }
+        logger.info("用户退出：" + sessionId + "当前在线人数为：" + ONLINE_COUNT.get());
+    }
+
+    @OnError
+    public void onError(Throwable error){
+    }
+
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+    }
+}
