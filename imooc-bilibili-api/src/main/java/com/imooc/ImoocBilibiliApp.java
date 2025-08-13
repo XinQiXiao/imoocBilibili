99c667afb72a6283f32db46eae10d8d83669b980
qixin <379281867@qq.com>
2025-08-13 23:32:15 +0800

    4-31

--- imooc-bilibili-api/src/main/java/com/imooc/ImoocBilibiliApp.java
+++ imooc-bilibili-api/src/main/java/com/imooc/ImoocBilibiliApp.java
@@ -4,8 +4,14 @@ import com.imooc.bilibili.service.websocket.WebSocketService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableAsync
+@EnableScheduling
 public class ImoocBilibiliApp {
 
     public static void main(String[] args){
--- imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/websocket/WebSocketService.java
+++ imooc-bilibili-service/src/main/java/com/imooc/bilibili/service/websocket/WebSocketService.java
@@ -12,6 +12,7 @@ import org.apache.rocketmq.common.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
@@ -115,6 +116,20 @@ public class WebSocketService {
 
     public void sendMessage(String message) throws IOException {
         this.session.getBasicRemote().sendText(message);
+    }
+
+    //或直接指定时间间隔，例如：5秒
+    @Scheduled(fixedRate=5000)
+    private void noticeOnlineCount() throws IOException {
+        for(Map.Entry<String, WebSocketService> entry : WebSocketService.WEBSOCKET_MAP.entrySet()){
+            WebSocketService webSocketService = entry.getValue();
+            if(webSocketService.session.isOpen()){
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("onlineCount", ONLINE_COUNT.get());
+                jsonObject.put("msg", "当前在线人数为" + ONLINE_COUNT.get());
+                webSocketService.sendMessage(jsonObject.toJSONString());
+            }
+        }
     }
 
     public Session getSession() {
